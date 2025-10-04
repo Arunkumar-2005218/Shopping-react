00c07504ed046f7f581493cea7679043b880f3c5
Arunkumar-2005218 <your.arunkumarsripathi9573@gmail.com>
2025-10-04 15:20:29 +0530

    Shopping React UI

---
+++ src/shoppingfolder/pages/MainPage.jsx
@@ -0,0 +1,30 @@
+
+
+import React,{useState} from 'react'
+import Header from '../components/Header'
+import Banner from'../components/Banner'
+import Collections from'../components/Collections'
+import Footer from'../components/Footer'
+import {Gents} from '../data'
+import { Ladies } from '../data'
+import WomanCollection from '../components/WomanCollection'
+
+const MainPage = () => {
+  const [gentsFashion] = useState(Gents)
+  const [ladiesFashion] = useState(Ladies)
+  console.log(Gents)
+  console.log(Ladies)
+
+  return (
+    <div>
+        <Header />
+        <Banner />
+        <Collections gentsFashion={gentsFashion} />
+      
+        <WomanCollection ladiesFashion= {ladiesFashion} />
+           <Footer />
+    </div>
+  )
+}
+
+export default MainPage
